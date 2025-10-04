b3a9b9a39fa7433399a41c41ae56661854f1085e
nikhil <nikhilyagik2004@gmail.com>
2025-10-04 12:38:06 +0530

    minor change

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import nodemailer from 'nodemailer';
 import cors from 'cors';
+import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -18,20 +18,15 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.post('/api/contact', async (req, res) => {
   const { name, email, message } = req.body;
-
-  if (!name || !email || !message) {
+  if (!name || !email || !message)
     return res.status(400).json({ message: 'All fields are required' });
-  }
-
-  const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
 
   try {
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
+    });
+
     await transporter.sendMail({
       from: email,
       to: process.env.EMAIL_USER,
@@ -48,7 +43,7 @@ app.post('/api/contact', async (req, res) => {
   }
 });
 
-app.get('/.*/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
