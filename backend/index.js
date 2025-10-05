0512c9f805d04954d5579408ae1543990f827882
nikhil <nikhilyagik2004@gmail.com>
2025-10-05 18:47:23 +0530

    changee

--- backend/index.js
+++ backend/index.js
@@ -45,14 +45,15 @@ app.post('/api/contact', async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
-    app.get(/(.*)/, (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-    
-  }
+if (process.env.NODE_ENV === 'production') {
+  const frontendPath = path.join(__dirname, '../frontend/dist');
+  app.use(express.static(frontendPath));
+
+
+  app.get('/(.*)/', (req, res) => {
+    res.sendFile(path.join(frontendPath, 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
