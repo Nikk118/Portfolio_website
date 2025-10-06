75338b6201e8bc6608aa37e963fdbc70e53da49e
nikhil <nikhilyagik2004@gmail.com>
2025-10-06 16:00:21 +0530

    again fixnig

--- backend/index.js
+++ backend/index.js
@@ -43,13 +43,12 @@ app.post('/api/contact', async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-}
+
   
 
 const PORT = process.env.PORT || 5000;
