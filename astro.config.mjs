7e69a1174e081ca417e50fc250679664aa54b647
nikhil <nikhilyagik2004@gmail.com>
2026-03-16 20:22:02 +0530

    logo added and seo done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
-import { defineConfig } from "astro/config";
+﻿import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://nikhil-yagik.netlify.app',
+  site: "https://nikhil-yagik.netlify.app",
   integrations: [tailwind()],
+  vite: {
+    optimizeDeps: {
+      include: ["@emailjs/browser"],
+    },
+  },
 });
