6306a1f2492c33cb69f3091376b8a8f61a748826
nikhil <nikhilyagik2004@gmail.com>
2026-03-16 16:37:35 +0530

    env configure

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: process.env.SITE_URL,
+  site: 'https://nikhil-yagik.netlify.app',
   integrations: [tailwind()],
 });
